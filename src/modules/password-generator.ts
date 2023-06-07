89d4b9c1365df7c29598d3ced419c7e0a7a132ad
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-06-07 20:10:04 +0200

    Update password-generator.ts

--- src/modules/password-generator.ts
+++ src/modules/password-generator.ts
@@ -9,3 +9,4 @@ const generatePassword = (
   let password = "";
   for (let i = 0; i < length; i++) {
     const randomIndex = Math.floor(Math.random() * charset.length);
+    password += charset[randomIndex];
